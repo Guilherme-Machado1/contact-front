cd9a51148501816ce1075b1c08c7defc3517c2d8
Guilherme Santos <guilhermemachado1900@gmail.com>
2022-02-03 12:47:57 -0300

    page show all contacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import {useState, useEffect} from "react";
-import { ContactContainer, ProfilePicture, InformationContainer, OwnerData, NavLink , Gif, H1} from "./styled";
-import logo from './teste.jpg'
+import { ContactContainer, InformationContainer, OwnerData, NavLink , Gif, H1} from "./styled";
 import Header from "../../Header";
 import gif from './257.gif';
 export default function Contacts() {
@@ -10,21 +9,17 @@ export default function Contacts() {
     async function getData() {
       try {
         const response = await axios.get('http://localhost:3001/contact');
-        // console.log(response.data.data)
         setContacts(response.data.data);
-
       } catch (error) {
-        console.log(error)
+        throw new Error(error)
       }
-      
     }
 
     getData();
   }, []);
 
 
-  async function handleDeleteContact (e, id, index) {
-    e.persist();
+  async function handleDeleteContact (id, index) {
     try {
       await axios.delete(`http://localhost:3001/contact/delete/${id}`);
       const newContacts = [...contacts];
@@ -36,52 +31,49 @@ export default function Contacts() {
   }
   
     return (
-        <>
-            <Header />
-            <H1>Todos Os Contatos</H1>
-               <ContactContainer >
-                 {contacts.map((contact, index) => (
-                    <InformationContainer key={String(contact.id)}>
-                        <div>
-                          <OwnerData>
-                            <ProfilePicture>
-                              <img alt="logo" width={69}  src={logo}/>
-                            </ProfilePicture>
-                            <p> {contact.Owner.name}</p>
-                            <p>{contact.Created_Time}</p>
-                          </OwnerData>
-                          <div>
-                              <NavLink to={`/contact/${contact.id}`}>
-                                <h2> {contact.Last_Name}</h2>
-                              </NavLink>
-                            </div>
-                          <div className="containerInfo">
-                              <label>Nome Completo:</label>
-                              <span> {contact.Full_Name}</span>
-                              <label>Email:</label>
-                              <span> {contact.Email || '-'}</span>
-                              <label>Telefone:</label>
-                              <span> {contact.Phone || '-'}</span>
-                              <label>Title:</label>
-                              <span> {contact.Title || '-'}</span>
-                              <label>Celular:</label>
-                              <span> {contact.Mobile || '-'}</span>
-                              <div className="options">
-                                  <NavLink to={`/contact/edit/${contact.id}`}>
-                                  <button className="edit"> Editar </button>
-                                  </NavLink>
-                                  <button className="delete" onClick={e => handleDeleteContact(e, contact.id, index)}> Excluir </button>
-                              </div>
-                            </div>
-                        </div>
+      <>
+        <Header />
+        <H1>Todos Os Contatos</H1>
+          <ContactContainer >
+          {contacts.map((contact, index) => (
+            <InformationContainer key={String(contact.id)}>
+              <div>
+                <OwnerData>
+                  <p> {contact.Owner.name}</p>
+                  <p>{contact.Created_Time}</p>
+                </OwnerData>
+                  <div>
+                    <NavLink to={`/contact/${contact.id}`}>
+                      <h2> {contact.Last_Name}</h2>
+                    </NavLink>
+                  </div>
+                  <div className="containerInfo">
+                    <label>Nome Completo:</label>
+                    <span> {contact.Full_Name}</span>
+                    <label>Email:</label>
+                    <span> {contact.Email || '-'}</span>
+                    <label>Telefone:</label>
+                    <span> {contact.Phone || '-'}</span>
+                    <label>Title:</label>
+                    <span> {contact.Title || '-'}</span>
+                    <label>Celular:</label>
+                    <span> {contact.Mobile || '-'}</span>
+                    <div className="options">
+                      <NavLink to={`/contact/edit/${contact.id}`}>
+                        <button className="edit"> Editar </button>
+                      </NavLink>
+                          <button className="delete" onClick={e => handleDeleteContact(contact.id, index)}> Excluir </button>
+                    </div>
+                  </div>
+              </div>
                     </InformationContainer>
                 ))}
-               </ContactContainer>
-               {contacts.length <= 0 &&
-                <Gif>
-                  <img src={gif} alt="loading"/>
-                </Gif>
-               }
-        </>
+          </ContactContainer>
+          {contacts.length <= 0 &&
+            <Gif>
+                <img src={gif} alt="loading"/>
+            </Gif>
+          }
+      </>
     )
 }
