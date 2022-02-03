7ecbb96591cdca258734590c0cbb340ae6e4cbf7
Guilherme Santos <guilhermemachado1900@gmail.com>
2022-02-03 12:51:05 -0300

    Router configuration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Contact from '../pages/Contact';
 import Contacts from '../pages/Contacts';
+import CreateContact from '../pages/CreactContact';
 import Edit from '../pages/Edit';
 
 export default function Router() {
@@ -10,6 +11,7 @@ export default function Router() {
                 <Route path="/" element={<Contacts /> } />
                 <Route path="/contact/:id" element={<Contact /> } />
                 <Route path="/contact/edit/:id" element={<Edit /> } />
+                <Route path="/contact/create" element={<CreateContact /> } />
             </Routes>
         </BrowserRouter>
     )
